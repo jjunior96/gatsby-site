5003d6d5da99fef37be9f8298d64639cebe114e1
Junior <jjalves96@gmail.com>
2020-05-31 11:46:36 -0300

    chore: page 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,8 +5,8 @@ import SEO from '../components/seo';
 const NotFoundPage = () => (
   <div>
     <SEO title="404: Not found" />
-    <h1>NOT </h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>NOT FOUND</h1>
+    <p>Ops... Nothing is here</p>
   </div>
 );
 
