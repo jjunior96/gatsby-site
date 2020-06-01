492e7153e72f896fa6e493e7a45e118eb86c46bc
Junior <jjalves96@gmail.com>
2020-06-01 18:38:24 -0300

    chore: add PropType into PostItem

--- src/components/PostItem/index.js
+++ src/components/PostItem/index.js
@@ -1,45 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import * as S from './styled';
 
-const PostItem = () => (
+const PostItem = ({ slug, background, category, date, title }) => (
   <S.PostItemWrapper>
     <S.PostItemCard>
-      <S.PostItemLink to="/">
-        <S.PostIemTag>TAG</S.PostIemTag>
+      <S.PostItemLink to={slug}>
+        <S.PostIemTag background={background}>{category}</S.PostIemTag>
       </S.PostItemLink>
       <S.PostItemLink to="/">
         <S.PostItemInfo>
-          <S.PostiItemTitle>Titulo</S.PostiItemTitle>
-          <S.PostItemDate>25 de Maio de 2020</S.PostItemDate>
-        </S.PostItemInfo>
-      </S.PostItemLink>
-    </S.PostItemCard>
-
-    <S.PostItemCard>
-      <S.PostItemLink to="/">
-        <S.PostIemTag>TAG</S.PostIemTag>
-      </S.PostItemLink>
-      <S.PostItemLink to="/">
-        <S.PostItemInfo>
-          <S.PostiItemTitle>Titulo</S.PostiItemTitle>
-          <S.PostItemDate>25 de Maio de 2020</S.PostItemDate>
-        </S.PostItemInfo>
-      </S.PostItemLink>
-    </S.PostItemCard>
-
-    <S.PostItemCard>
-      <S.PostItemLink to="/">
-        <S.PostIemTag>TAG</S.PostIemTag>
-      </S.PostItemLink>
-      <S.PostItemLink to="/">
-        <S.PostItemInfo>
-          <S.PostiItemTitle>Titulo</S.PostiItemTitle>
-          <S.PostItemDate>25 de Maio de 2020</S.PostItemDate>
+          <S.PostiItemTitle>{title}</S.PostiItemTitle>
+          <S.PostItemDate>{date}</S.PostItemDate>
         </S.PostItemInfo>
       </S.PostItemLink>
     </S.PostItemCard>
   </S.PostItemWrapper>
 );
+
+PostItem.propTypes = {
+  slug: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+};
 
 export default PostItem;
